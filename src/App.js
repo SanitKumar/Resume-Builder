4a53e2c7f3979c4550797bb80e3d5136520f986c
Sanit Kumar <sanitkumar2506@gmail.com>
2024-01-29 20:21:40 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <title>Resume Builder - Create Professional Resumes Online</title>
           <meta name="description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
           <meta name="keywords" content="resume builder, professional resumes, online resumes, resume templates" />
-          <meta name="author" content="Hardik Desai" />
+          <meta name="author" content="Sanit Kumar" />
           <meta property="og:title" content="Resume Builder - Create Professional Resumes Online" />
           <meta property="og:description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
           <meta property="og:image" content="https://avatars.githubusercontent.com/u/87645745?v=4" />
